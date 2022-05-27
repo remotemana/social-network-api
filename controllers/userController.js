eb2fc9bce5e0906e0ee4a88b4f5ee62e6ddca512
Lucas Roman <lucas.e.roman@gmail.com>
2022-05-26 23:44:45 -0700

    working on making sure routes are operational, delete a friend is being weird

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,5 +64,33 @@ module.exports = {
         )
         .catch ((err)=> res.status(500).json(err));
       },
-  
+
+      createUserFriend(req,res){
+        User.findOneAndUpdate(
+          {_id:req.params.userId},
+          {$addToSet: {friends: req.body} },
+          { runValidators: true, new: true }
+        )
+        .then((user)=>
+        !user
+        ? res.status(404).json({message: "No user with this id!"})
+        : res.json(user)
+        )
+        .catch((err)=> res.status(500).json(err))
+      },
+
+      deleteUserFriend(req, res) {
+
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friend:{friendId: req.params.friendId } } },
+            { runValidators: true, new: true }
+          )
+            .then((user) =>
+              !user
+                ? res.status(404).json({ message: 'No user with this id!' })
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+        },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,6 +13,10 @@ const {
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:usertId
-router.route('/:userId').get(getSingleUser).put(updateUser)
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends').post(createUserFriend)
+
+router.route('/:userId/friends/:friendsId').delete(deleteUserFriend)
 
 module.exports = router;
